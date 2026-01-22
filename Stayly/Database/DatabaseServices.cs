9167374890b3cf16cd828331e485ca66dc427cc7
MartinCsupor <csupormartin@turr.hu>
2026-01-22 11:35:05 +0100

    adatbazis kapcsolat

---
+++ Stayly/Database/DatabaseServices.cs
@@ -0,0 +1,50 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stayly.Database
+{
+    internal class DatabaseServices
+    {
+        private static string connectionString;
+        private static string table;
+        private static string query_parameters;
+
+        public static void DBConnectionCheck(string connectionString)
+        {
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+                    Console.WriteLine("Sikeres kapcsolodas");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Sikertelen kapcsolodas");
+                Console.WriteLine(ex);
+            }
+        }
+
+        public static DataTable getAllData(string connectionString, string table)
+        {
+            using var connection = new MySqlConnection(connectionString);
+            connection.Open();
+
+            using var command = new MySqlCommand($"SELECT * FROM {table}", connection);
+
+            using var reader = command.ExecuteReader();
+
+            var dataTable = new DataTable();
+            dataTable.Load(reader);
+
+            return dataTable;
+        }
+    }
+}
--- Stayly/Program.cs
+++ Stayly/Program.cs
@@ -1,7 +1,27 @@
-﻿internal class Program
+﻿using Stayly.Database;
+using Stayly.Model;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
+
+internal class Program
 {
+    public static readonly string connectionString = "Server=localhost;Database=stayly;User=root";
+    public static DataTable adatok = new DataTable();
+    public static List<Szallas> szallasList = new List<Szallas>();
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        DatabaseServices.DBConnectionCheck(connectionString);
+        SelectAll(connectionString, "szallas");
+        SzallasFeltoltes(adatok);
+    }
+
+    private static void SzallasFeltoltes(DataTable adatok)
+    {
+
+    }
+
+    private static void SelectAll(string connectionString, string v)
+    {
+        adatok = DatabaseServices.getAllData(connectionString, "szallas");
     }
 }
